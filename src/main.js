7562e8710a538f04942c1d8be10a7856af79d343
Oscar <cerdeiros@gmail.com>
2025-11-14 15:15:46 +0100

    Avoid negative values

--- src/main.js
+++ src/main.js
@@ -14,11 +14,17 @@ function updateCountdown() {
   const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
   // Actualizar elementos HTML
+  if (diff <= 6000) {
+    document.getElementById('days').textContent = '00';
+    document.getElementById('hours').textContent = '00';
+    document.getElementById('minutes').textContent = '00';
+    document.getElementById('seconds').textContent = '00';
+  } else {  
   document.getElementById('days').textContent = String(days).padStart(2, '0');
   document.getElementById('hours').textContent = String(hours).padStart(2, '0');
   document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
   document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
-
+  }
   // Mostrar mensaje cuando se llegue
   const messageEl = document.getElementById('message');
   if (diff <= 0) {
